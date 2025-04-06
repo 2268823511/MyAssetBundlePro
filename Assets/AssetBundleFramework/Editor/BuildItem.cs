3bcb20f96de0b2b4ca7ef1b0c238ca27272a517f
2268823511 <2268823511@qq.com>
2025-04-06 23:49:14 +0800

    “提交客制化AB管理”

---
+++ Assets/AssetBundleFramework/Editor/BuildItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace AssetBundleFramework
+{
+    /// <summary>
+    /// 高度定制的每一个打包文件夹的具体信息,资源类型,资源路径等
+    /// </summary>
+    public class BuildItem
+    {
+        [DisplayName("资源路径")]
+        [XmlAttribute("AssetPath")]
+        public string assetPath { get; set; }
+
+        [DisplayName("资源类型")]
+        [XmlAttribute("ResourceType")]
+        public EResourceType resourceType { get; set; } = EResourceType.Direct;
+
+        [DisplayName("ab粒度类型")]
+        [XmlAttribute("BundleType")]
+        public EBundleType bundleType { get; set; } = EBundleType.File;
+
+        [DisplayName("资源后缀")]
+        [XmlAttribute("Suffix")]
+        public string suffix { get; set; } = ".prefab"; //Suffix资源配置里面的字符串
+
+        [XmlIgnore]
+        public List<string> ignorePaths { get; set; } = new List<string>(); 
+
+        [XmlIgnore]
+        public List<string> suffixes { get; set; } = new List<string>(); //资源类型,主要是有些包体可以多种类型打在一起,例如Suffix=".png|.spriteatlas"
+
+        //匹配该打包设置的个数
+        [XmlIgnore]
+        public int Count { get; set; }  //资源类型个数
+    }
+}
---
+++ Assets/AssetBundleFramework/Editor/XmlUtility.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace AssetBundleFramework
+{
+    /// <summary>
+    /// 序列化,反序列化XML,解析路径BuildSetting
+    /// </summary> <summary>
+    /// 
+    /// </summary>
+    public static class XmlUtility
+    {
+        public static UTF8Encoding UTF8 = new UTF8Encoding(false);
+
+        /// <summary>
+        /// 序列化XML配置文件路径
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName">配置文件--文件路径</param>
+        /// <param name="data">应该是策划配置的配置文件的一个表?</param>
+        /// <returns></returns> <summary>
+        public static bool Save<T>(string fileName, T data)
+        {
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            FileStream stream = File.Open(fileName, FileMode.Create, FileAccess.Write);
+            //去掉命名空间
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            XmlTextWriter writer = new XmlTextWriter(stream, UTF8);
+            writer.Formatting = Formatting.Indented;
+            serializer.Serialize(writer, data, ns);
+            writer.Close();
+            stream.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// 反序列化XML配置文件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static T Read<T>(string fileName) where T : class
+        {
+            FileStream stream = null;
+            if (!File.Exists(fileName))
+            {
+                return default(T);
+            }
+
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                stream = File.OpenRead(fileName);
+                XmlReader reader = XmlReader.Create(stream);
+                T instance = (T)serializer.Deserialize(reader);
+                stream.Close();
+                return instance;
+            }
+            catch
+            {
+                if (stream != null)
+                    stream.Close();
+                return default(T);
+            }
+        }
+
+        public static string ObjectToString(Object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            XmlTextWriter writer = new XmlTextWriter(stream, UTF8);
+            writer.Formatting = Formatting.Indented;
+
+            serializer.Serialize(writer, obj);
+
+            stream.Seek(0, SeekOrigin.Begin);
+            StreamReader sr = new StreamReader(stream);
+            string xmlString = sr.ReadToEnd();
+            sr.Close();
+
+            return xmlString;
+        }
+
+        public static T StringToObject<T>(string text) where T : class
+        {
+            byte[] byteArray = UTF8.GetBytes(text);
+            MemoryStream stream = new MemoryStream(byteArray);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(stream);
+        }
+    }
+}
